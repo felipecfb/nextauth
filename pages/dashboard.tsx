ac4cda05755b71c441e7024cc43cfba423ba201b
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-27 12:32:44 -0300

    feat: recuperando estado da autenticação

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { api } from "../services/api";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
-  return (
-    <h1>Dashboard: {user?.email}</h1>
-  );
+  useEffect(() => {
+    api.get("/me").then((response) => console.log(response));
+  });
+
+  return <h1>Dashboard: {user?.email}</h1>;
 }
