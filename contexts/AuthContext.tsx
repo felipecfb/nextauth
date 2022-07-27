5ca98eec8d0d06e4f4d6e7002933083376e9f7cf
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-07-27 12:11:58 -0300

    feat: salvando tokens nos cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,12 +1,13 @@
 import Router from "next/router";
 import { createContext, ReactNode, useState } from "react";
+import { setCookie } from "nookies";
 import { api } from "../services/api";
 
 type User = {
   email: string;
   permissions: string[];
   roles: string[];
-}
+};
 
 type SignInCredentials = {
   email: string;
@@ -38,13 +39,23 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
       const { token, refreshToken, permissions, roles } = response.data;
 
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
       setUser({
         email,
         permissions,
-        roles
+        roles,
       });
 
-      Router.push('/dashboard');
+      Router.push("/dashboard");
     } catch (error) {
       console.log(error);
     }
